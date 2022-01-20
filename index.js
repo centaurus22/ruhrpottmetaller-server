94b71b8d336ea5a20fa4394fc8a9c4ae7b72e5f6
Klaus Quintern <klaus.quintern@violution.net>
2022-01-20 15:57:25 +0100

    Initial Commit
    Create a basic node.js server.

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.get('*', (request, response) => {
+        response.send('Response to a get request');
+});
+
+app.all('*', (request, response) => {
+        response.status(501).send({msg: 'Not Implemented'});
+});
+
+const port = 3000;
+app.listen(port, () => console.log('Server running on port ' + port));
