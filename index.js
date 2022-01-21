52db60700e86f4db681d4c48b35403b77a4ad398
Klaus Quintern <klaus.quintern@violution.net>
2022-01-21 12:47:44 +0100

    Rename the variable which contains express.
    Rename the variable which contains express to "webserver".

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 'use strict';
 
 const express = require('express');
-const app = express();
-const database = require('./lib/database.js');
+const webserver = express();
+//const database = require('./lib/database.js');
 
-app.get('*', (request, response) => {
+webserver.get('*', (request, response) => {
         response.send('Response to a get request');
 });
 
-app.all('*', (request, response) => {
+webserver.all('*', (request, response) => {
         response.status(501).send({msg: 'Not Implemented'});
 });
 
 const port = 3000;
-app.listen(port, () => console.log('Server running on port ' + port));
+webserver.listen(port, () => console.log('Server running on port ' + port));
