d2687b221ded6bed5816692a844766332c226a71
Klaus Thorres <ruhrpottmetaller@freenet.de>
2022-04-16 11:43:36 +0200

    Add a error handler for internal server errors.

--- index.js
+++ index.js
@@ -9,5 +9,9 @@ app.use(express.static('static'));
 
 app.use('/api/v1/event', event);
 
+app.use((error, request, response, next) => {
+  response.status(500).send('Internal Server Error');
+});
+
 const port = 3000;
 app.listen(port, () => console.log('Server running on port ' + port));
