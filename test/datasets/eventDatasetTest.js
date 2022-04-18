5408931a78531e16d37d19f02b45084ae5f03208
Klaus Thorres <ruhrpottmetaller@freenet.de>
2022-04-18 13:30:52 +0200

    Introduce the model and define first functions.
    Introduce the model and move the dataset examination functions to it.

--- test/datasets/eventDatasetTest.js
+++ test/datasets/eventDatasetTest.js
@@ -1,9 +1,8 @@
 const assert = require('assert');
 const event = require('../../lib/datasets/eventDataset.js');
-const abstractDataset = require('../../lib/datasets/abstractDataset');
 
 suite('dataset/event.js', function() {
-   test('should return a class', function() {
+   test('should return an object', function() {
       let event_1 = new event();
       assert.equal(typeof event_1, 'object');
   });
@@ -15,68 +14,4 @@ suite('dataset/event.js', function() {
       assert.equal(event_2.getUrl() instanceof Date, true)
   });
 
-  test(
-    'getProperties() of a Dataset with one property should return an Array with the property name without _',
-    function() {
-      const dataset = class dataset extends abstractDataset{
-        _url = null;
-      };
-      const Dataset = new dataset();
-      assert.equal(Dataset.getProperties() instanceof Array, true);
-      assert.equal(Dataset.getProperties().length, 1);
-      assert.equal(Dataset.getProperties()[0], 'url');
-  });
-
-  test(
-    'getProperties() of a Dataset with properties should return an Array with property names without _',
-    function() {
-      const dataset = class dataset extends abstractDataset{
-        _url = null;
-        _name = null;
-      };
-      const Dataset = new dataset();
-      assert.equal(Dataset.getProperties() instanceof Array, true);
-      assert.equal(Dataset.getProperties().length, 2);
-      assert.equal(Dataset.getProperties()[0], 'url');
-      assert.equal(Dataset.getProperties()[1], 'name');
-  });
-
-  test('getDatasetName() should return the name of the dataset', function() {
-    const Event = new event(new Array);
-    assert.equal(Event.getDatasetName(), 'event');
-  });
-
-  test(
-    'getDatasetName() should return the name of the testDataset',
-    function() {
-      const testDataset = class testDataset extends abstractDataset{};
-      const TestDataset = new testDataset();
-      assert.equal(TestDataset.getDatasetName(), 'test');
-  });
-
-  test(
-    'getDatasetName() should throw an error if the object name does not end with "Dataset"',
-    function() {
-      const testData = class testData extends abstractDataset{};
-      const TestData = new testData();
-      assert.throws(
-        () => TestData.getDatasetName(),
-        Error,
-        'The Dataset name must end with "Dataset".'
-      );
-  });
-
-  test(
-    'getDatasetName() should throw an error if a dataset property name less than 2 chars long',
-    function() {
-      const testDataset = class testDataset extends abstractDataset{
-        _ = null;
-      };
-      const TestDataset = new testDataset();
-      assert.throws(
-        () => TestDataset.getProperties(),
-        Error,
-        'Property names must contain at least two chars including the underscore."'
-      );
-  });
 });
---
+++ test/modelTest.js
@@ -0,0 +1,72 @@
+const model = require('../lib/model.js');
+const assert = require('assert');
+
+suite('model.js', function() {
+  test(
+    '_getProperties() of a Dataset with one property should return an Array with the property name without _',
+    function() {
+      const dataset = class dataset {
+        _url = null;
+      };
+      const Model = new model(new dataset);
+      assert.equal(Model._getProperties() instanceof Array, true);
+      assert.equal(Model._getProperties().length, 1);
+      assert.equal(Model._getProperties()[0], 'url');
+  });
+
+  test(
+    '_getProperties() of a Dataset with properties should return an Array with property names without _',
+    function() {
+      const dataset = class dataset {
+        _url = null;
+        _name = null;
+      };
+      const Model = new model(new dataset);
+      assert.equal(Model._getProperties() instanceof Array, true);
+      assert.equal(Model._getProperties().length, 2);
+      assert.equal(Model._getProperties()[0], 'url');
+      assert.equal(Model._getProperties()[1], 'name');
+  });
+
+  test('_getDatasetName() should return the name of the dataset', function() {
+    const eventDataset = class eventDataset {
+      _url = null;
+    };
+    const Model = new model(new eventDataset);
+    assert.equal(Model._getDatasetName(), 'event');
+  });
+
+  test(
+    '_getDatasetName() should return the name of the testDataset',
+    function() {
+      const testDataset = class testDataset {};
+      const Model = new model( new testDataset());
+      assert.equal(Model._getDatasetName(), 'test');
+  });
+
+  test(
+    '_getDatasetName() should throw an error if the object name does not end with "Dataset"',
+    function() {
+      const eventData = class eventData {};
+      const Model = new model(new eventData());
+      assert.throws(
+        () => TestData._getDatasetName(),
+        Error,
+        'The Dataset name must end with "Dataset".'
+      );
+  });
+
+  test(
+    '_getDatasetName() should throw an error if a dataset property name less than 2 chars long',
+    function() {
+      const testDataset = class testDataset {
+        _ = null;
+      };
+      const Model = new model(new testDataset());
+      assert.throws(
+        () => Model._getProperties(),
+        Error,
+        'Property names must contain at least two chars including the underscore."'
+      );
+  });
+});
