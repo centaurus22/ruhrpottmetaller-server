bf5c69e76f031d4611bd681503c8a797dcfff799
Klaus Thorres <ruhrpottmetaller@freenet.de>
2022-06-17 18:53:27 +0200

    Revert "Remove the value from the variable object."
    This reverts commit bcf3e01b60f6868cb617956026ac0aa8fa7040f6.

--- test/variableTest.js
+++ test/variableTest.js
@@ -2,6 +2,21 @@ const assert = require('assert');
 const variable = require('../lib/variable.js');
 
 describe('variable.js', function() {
+  describe('setValue()', function () {
+    it('should accept a value', function() {
+      const variable_1 = new variable();
+      variable_1.setValue(null);
+    });
+  });
+
+  describe('getValue()', function () {
+    it('should return the previous set value', function() {
+      const variable_1 = new variable();
+      variable_1.setValue('foo');
+      assert.equal(variable_1.getValue(), 'foo');
+    });
+  });
+
   describe('getDatabaseColumnName()', function () {
     it('should return the value which is passed by the constructor', function() {
       const variable_1 = new variable('bar');
