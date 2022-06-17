59e57cb8796d76e9d5b33421d474a3a17fe9c2b1
Klaus Thorres <ruhrpottmetaller@freenet.de>
2022-06-17 17:35:20 +0200

    Move the dataset examination functions.
    Move the dataset examination functions from the model class to the
    dataset class. Because the model should not retrieve internal values
    from the dataset object directly.

--- test/datasets/eventDatasetTest.js
+++ test/datasets/eventDatasetTest.js
@@ -3,14 +3,32 @@ const event = require('../../lib/datasets/eventDataset.js');
 
 describe('dataset/eventDataset.js', function() {
   it('should define an object', function() {
-    let event_1 = new event();
-    assert.equal(typeof event_1, 'object');
+    let Event = new event();
+    assert.equal(typeof Event, 'object');
   });
 
   describe ('getUrl()', function () {
     it('should return an object which is passed by the constructor', function () {
-      let event_2 = new event(new Date());
-      assert.equal(event_2.getUrl() instanceof Date, true);
+      let Event = new event('https://www.google.de')
+      assert.equal(Event.getUrl(), 'https://www.google.de');
+    });
+  });
+
+  describe('getProperties()', function () {
+    it(
+      'should return an Array with the property name "_url"',
+      function () {
+        let Event = new event();
+        assert.equal(Event.getProperties() instanceof Array, true);
+        assert.equal(Event.getProperties()[0], '_url');
+
+    });
+  });
+
+  describe('getDatasetName()', function () {
+    it('should return the name of the dataset', function () {
+      let Event = new event();
+      assert.equal(Event.getDatasetName(), 'event');
     });
   });
 });
