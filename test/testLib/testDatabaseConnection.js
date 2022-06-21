40ecf2b2896602b01a728a9c2f1e2f48d057c28c
Thorres <63589495+klaus-thorres@users.noreply.github.com>
2022-06-21 16:25:40 +0200

    Delete testDatabaseConnection.js
    Remove user data.

--- test/testLib/testDatabaseConnection.js
+++
@@ -1,9 +0,0 @@
-const mysql = require('mysql');
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'ruhrpottmetaller',
-  database: 'ruhrpottmetaller_dev',
-  password: 'aware impulsive profusely'
-});
-
-module.exports = connection;
