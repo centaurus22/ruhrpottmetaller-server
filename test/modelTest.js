e919fb99e8685f7040e09e83d81ef0549d827a43
Klaus Thorres <ruhrpottmetaller@freenet.de>
2022-06-21 15:15:49 +0200

    Ignore events which are in the past.

--- test/modelTest.js
+++ test/modelTest.js
@@ -31,7 +31,7 @@ describe('model.js', function() {
     });
 
     it('should return an Array with one Dataset if event table contains one item', async function() {
-      const sql = 'INSERT INTO event SET url = "https://www.ruhrpottmetaller.de"';
+      const sql = 'INSERT INTO event SET date_start = CURRENT_DATE() + 1, url = "https://www.ruhrpottmetaller.de"';
       await testDatabaseConnection.query(sql, function (error) {
         if (error) {
           throw error;
@@ -53,8 +53,8 @@ describe('model.js', function() {
     });
 
     it('should return an Array with two Datasets if event table contains two items', async function() {
-      const sql1 = 'INSERT INTO event SET url = "https://www.beerfest.de"';
-      const sql2 = 'INSERT INTO event SET url = "https://www.ruhrpottmetaller.de"';
+      const sql1 = 'INSERT INTO event SET date_start = CURRENT_DATE() + 1, url = "https://www.beerfest.de"';
+      const sql2 = 'INSERT INTO event SET date_start = CURRENT_DATE() + 1, url = "https://www.ruhrpottmetaller.de"';
       await testDatabaseConnection.query(sql1, function (error) {
         if (error) {
           throw error;
@@ -82,7 +82,7 @@ describe('model.js', function() {
       );
     });
 
-    it('should return sort datasets by date', async function() {
+    it('should sort datasets by date', async function() {
       const sql1 = 'INSERT INTO event SET date_start = "2032-07-22", url = "https://www.beerfest.de"';
       const sql2 = 'INSERT INTO event SET date_start = "2032-06-22", url = "https://www.ruhrpottmetaller.de"';
       await testDatabaseConnection.query(sql1, function (error) {
@@ -104,6 +104,27 @@ describe('model.js', function() {
         datasets[0].getUrl().getValue(),
         'https://www.ruhrpottmetaller.de'
       );
+    });
+
+    it('should ignore datasets in the past', async function() {
+      const sql1 = 'INSERT INTO event SET date_start = "2012-07-22", url = "https://www.beerfest.de"';
+      const sql2 = 'INSERT INTO event SET date_start = "2032-06-22", url = "https://www.ruhrpottmetaller.de"';
+      await testDatabaseConnection.query(sql1, function (error) {
+        if (error) {
+          throw error;
+        }
+      });
+      await testDatabaseConnection.query(sql2, function (error) {
+        if (error) {
+          throw error;
+        }
+      });
+      const datasets = await Model.getDatasets();
+      assert.equal(
+        datasets[0].getUrl().getValue(),
+        'https://www.ruhrpottmetaller.de'
+      );
+      assert.equal(datasets.length, 1);
     });
   });
 });
