ca70bf7a6b32d8917cac1d61a256d57cffa4efcf
Klaus Thorres <ruhrpottmetaller@freenet.de>
2022-06-24 17:57:27 +0200

    Remove the controller object from the chain.
    Because it just routes the data from the model anyway.

--- routes/event.js
+++ routes/event.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const controller = require('../lib/controller');
 const databaseConnection = require('../lib/databaseConnection');
 databaseConnection.connect();
-const model = require('../lib/model')
+const model = require('../lib/model');
 const eventDataset = require('../lib/datasets/eventDataset')
 const variable = require('../lib/variable')
-const Controller = new controller(new model(databaseConnection, new eventDataset(
+const Model = new model(databaseConnection, new eventDataset(
   new variable('id'),
   new variable('name'),
   new variable('date_start'),
@@ -15,10 +14,10 @@ const Controller = new controller(new model(databaseConnection, new eventDataset
   new variable('venue_id'),
   new variable('url'),
   new variable('sold_out')
-)));
+));
 
 router.get('/', async (request, response, next) => {
-  response.status(200).json(await Controller.getResponse(next));
+  response.status(200).json(await Model.getDatasets(next));
 });
 
 router.use((error, request, response, next) => {
